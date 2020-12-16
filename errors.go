be45c91fe5f0cdba43274d9ad56c202827a74cc1
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-16 14:13:55 -0600

    Update errors.go

--- errors.go
+++ errors.go
@@ -1,3 +1,5 @@
+// (c) 2020 Gon Y Yi. <https://gonyyi.com/copyright.txt>
+
 package cache
 
 const (
@@ -5,13 +7,13 @@ const (
 	//
 	// =====================================================================================================================
 	ERR_DIR_NOT_FOUND = "directory not exist"
-
+	ERR_NIL_LOG       = "nil is not expected log"
 
 	// =====================================================================================================================
 	// FORMATTED ERROR STRING
 	// =====================================================================================================================
-	ERRF_CANNOT_OPEN_X  = "cannot open <%s>"
-	ERRF_CANNOT_FIND_A_QUERY_X  = "cannot find a query file <%s>"
-	ERRF_VAR_X_NOT_FOUND = "query var <%s> not found"
-	ERRF_BAD_STATUS_CODE_X = "bad status code <%d>"
+	ERRF_CANNOT_OPEN_X         = "cannot open <%s>"
+	ERRF_CANNOT_FIND_A_QUERY_X = "cannot find a query file <%s>"
+	ERRF_VAR_X_NOT_FOUND       = "query var <%s> not found"
+	ERRF_BAD_STATUS_CODE_X     = "bad status code <%d>"
 )
