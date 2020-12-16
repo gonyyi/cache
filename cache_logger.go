ec1f75dcbbc6417908c3162d4d82b5a728817d5e
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-16 13:55:46 -0600

    Update cache_logger.go

--- cache_logger.go
+++ cache_logger.go
@@ -1,60 +1,11 @@
+// (c) 2020 Gon Y Yi. <https://gonyyi.com/copyright.txt>
+
 package cache
 
-import (
-	"github.com/gonyyi/alog"
-	"io"
-)
-
-type cacheLogger interface {
+type Logger interface {
 	Debugf(string, ...interface{})
 	Infof(string, ...interface{})
 	Warnf(string, ...interface{})
 	Errorf(string, ...interface{})
 	Fatalf(string, ...interface{})
-}
-
-// =====================================================================================================================
-// CUSTOM -- ALOG adopter
-// =====================================================================================================================
-
-// Create custom level
-const (
-	LvDEBUG alog.Level = 1 << iota
-	LvINFO
-	LvWARN
-	LvERROR
-	LvFATAL
-	LvALL = LvDEBUG | LvINFO | LvWARN | LvERROR | LvFATAL
-)
-
-func NewCacheLog(out io.Writer) *cacheLog {
-	c := cacheLog{}
-	c.SetPrefix("Cache ")
-	c.SetFlag(alog.F_STD)
-	c.SetOutput(out)
-	c.LvOverride(LvALL)
-	c.LvDisable(LvDEBUG)
-	return &c
-}
-
-type cacheLog struct {
-	alog.ALogger
-}
-
-func (c *cacheLog) Debugf(fmt string, a ...interface{}) {
-	c.Printfl(LvDEBUG, "[DEBUG] "+fmt, a...)
-}
-func (c *cacheLog) Infof(fmt string, a ...interface{}) {
-	c.Printfl(LvINFO, "[INFO]  "+fmt, a...)
-}
-func (c *cacheLog) Warnf(fmt string, a ...interface{}) {
-	c.Printfl(LvWARN, "[WARN]  "+fmt, a...)
-}
-func (c *cacheLog) Errorf(fmt string, a ...interface{}) {
-	c.Printfl(LvERROR, "[ERROR] "+fmt, a...)
-}
-func (c *cacheLog) Fatalf(fmt string, a ...interface{}) {
-	c.Printfl(LvERROR, "[FATAL] "+fmt, a...)
-	// Terminate program
-	// os.Exit(1)
 }
