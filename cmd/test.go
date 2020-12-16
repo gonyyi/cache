ef73ef71c559cb68d610eb90c1656e3502944785
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-16 14:12:27 -0600

    Update test.go

--- cmd/test.go
+++ cmd/test.go
@@ -1,18 +1,27 @@
+// (c) 2020 Gon Y Yi. <https://gonyyi.com/copyright.txt>
+
 package main
 
 import (
+	"github.com/gonyyi/alog"
+	"github.com/gonyyi/alog/alogc"
 	"github.com/gonyyi/cache"
+	"os"
 )
 
 func main() {
 	// create new config file
-	// err := cache.NewConfig("./config.json", true)
+	// err := cache.cache.CreateNewConfig("./config.json", true)
 
-	c, err := cache.New("./config.json")
+	c := cache.New()
+	l := alogc.New(os.Stderr, "", alog.F_STD)
+	c.SetLogger(l)
+
+	err := c.Open("./config.json")
 	if err != nil {
 		println(err.Error())
 	}
-	c.CachePull("test")
-	c.Save()
 
+	c.CachePullAll()
+	c.Save()
 }
