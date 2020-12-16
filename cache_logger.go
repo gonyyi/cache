95ad2b8515af5859bba7edc6f7dd4b6a3831f4f1
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-16 14:13:29 -0600

    Update cache_logger.go

--- cache_logger.go
+++ cache_logger.go
@@ -9,3 +9,19 @@ type Logger interface {
 	Errorf(string, ...interface{})
 	Fatalf(string, ...interface{})
 }
+
+// =====================================================================================================================
+// DEFAULT DUMMY LOGGER
+// =====================================================================================================================
+type dummyLog struct {}
+
+func(d *dummyLog) Debugf(a string, b ...interface{}){}
+func(d *dummyLog) Infof(a string, b ...interface{}){}
+func(d *dummyLog) Warnf(a string, b ...interface{}){}
+func(d *dummyLog) Errorf(a string, b ...interface{}){}
+func(d *dummyLog) Fatalf(a string, b ...interface{}){}
+
+func newDummyLogger() *dummyLog {
+	return &dummyLog{}
+}
+
